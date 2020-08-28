e9ff2a69ca44a9f5e9e687e83ea011d8d532a86d
Cadimo Duarte <cadimo.junior@zup.com.br>
2020-08-28 16:08:23 -0300

    formatacao

--- eks/provider.tf
+++ eks/provider.tf
@@ -1,15 +1,15 @@
 provider "aws" {
   version = ">= 2.28.1"
-  profile = "zup-charles-training" 
-  region = "sa-east-1"
+  profile = "zup-charles-training"
+  region  = "sa-east-1"
 }
 
 terraform {
-   required_version = ">= 0.12.0"
-   backend "s3" {
-    bucket = "charles-training-terraform"
-    region = "sa-east-1"
-    key = "terraform.tfstate"
+  required_version = ">= 0.12.0"
+  backend "s3" {
+    bucket  = "charles-training-terraform"
+    region  = "sa-east-1"
+    key     = "terraform.tfstate"
     profile = "zup-charles-training"
   }
 }
--- eks/variables.tf
+++ eks/variables.tf
@@ -1,20 +1,20 @@
 variable "region" {
   default = "sa-east-1"
-  type = string
+  type    = string
 }
 
 variable "cluster_name" {
   default = "eks-training"
-  type = string
+  type    = string
 }
 
 variable "subnets" {
-  default = ["subnet-6149c428","subnet-7869e61e","subnet-7bd24720"]
-  type = list(string)
+  default = ["subnet-6149c428", "subnet-7869e61e", "subnet-7bd24720"]
+  type    = list(string)
 }
 
 variable "vpc_id" {
   default = "vpc-089d8f6f"
-  type = string
+  type    = string
 }
 
